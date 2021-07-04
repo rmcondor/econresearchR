0eb71e12bb4ee13daa655e96a4bc22c1b8a72308
Andy Rufasto <andy@andyrufasto.cf>
2021-07-04 11:38:39 -0500

    Use lapply to create folder

--- R/econresearchR.R
+++ R/econresearchR.R
@@ -4,27 +4,12 @@ econ_research <- function(directory){
   project_txt <- "Overview of your research project"
   writeLines(project_txt, "readme.txt")
 
-  core_folders <- c("01_administration","02_literature", "03_data", "04_dissemination")
-  for (j in 1:length(core_folders)){
-    dir.create(core_folders[j])}
 
-  literature <- paste(directory, "02_literature", sep = "/")
-  literature_folders <- c("01_books","02_papers", "03_others")
-  for (j in 1:length(literature_folders)){
-    dir.create(paste(literature, literature_folders[j], sep = "/"))
-  }
+core_folders <- c("01_administration",
+	"02_literature/01_books","02_literature/02_papers", "02_literature/03_others",
+	"03_data/01_raw","03_data/02_codes", "03_data/03_cleaned", "03_data/04_analysis","03_data/05_results",
+ 	"04_dissemination/01_presentations", "04_dissemination/02_publication") 
 
-  data <- paste(directory,"03_data", sep="/")
-
-  data_folders <- c("01_raw","02_codes", "03_cleaned", "04_analysis","05_results")
-  for (j in 1:length(data_folders)){
-    dir.create(paste(data, data_folders[j], sep = "/"))
-  }
-
-  diss <- paste(directory, "04_dissemination", sep="/")
-  diss_folders <- c("01_presentations","02_publication")
-  for (j in 1:length(diss_folders)){
-    dir.create(paste(diss, diss_folders[j],sep="/"))
-  }
+lapply(core_folders,dir.create,recursive = TRUE)
 
 }
